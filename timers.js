7d5ba6f12e7a1bfd383cafecd1ff64caed963f23
limzkil <78449568+limzkil@users.noreply.github.com>
2021-12-06 09:53:34 -0500

    Set up main for students

--- timers.js
+++ timers.js
@@ -13,55 +13,53 @@ stopTimeOutButton.style.pointerEvents = 'none';
 //adding a click event listener to our start  time out button; the callback function is what occurs on click
 startTimeOutButton.addEventListener('click', () => {
   //enabling the stop button
-  stopTimeOutButton.style.pointerEvents = 'auto';
+
   //disabling the start button to prevent multiple timers occurring
-  startTimeOutButton.style.pointerEvents = 'none';
+
   //alerting the user that the time out has begun
-  timeOutDisplay.textContent = 'I am timing out...';
+
 
   /*reassigning global variable to start the time out,
     function executes after 5 seconds*/
-  timeOutTimer = setTimeout(() => {
-    //disabling the stop button
-    stopTimeOutButton.style.pointerEvents = 'none';
-    //enabling the start button
-    startTimeOutButton.style.pointerEvents = 'auto';
-    //alerting the user that the time out is complete
-    timeOutDisplay.textContent = 'I timed out at 5 seconds!';
-  }, 5000);
+
+  //disabling the stop button
+
+  //enabling the start button
+
+  //alerting the user that the time out is complete
+
 });
 
 //adding a click event listener to our stop time out button; the callback function is what occurs on click
 stopTimeOutButton.addEventListener('click', () => {
   //disabling the stop button
-  stopTimeOutButton.style.pointerEvents = 'none';
+
   //enabling the start button
-  startTimeOutButton.style.pointerEvents = 'auto';
+
   //alerting the user that the time out has been stopped
-  timeOutDisplay.textContent = 'I am no longer timing out!';
+
   //stop the time out with the start variable as its argument
-  clearTimeout(timeOutTimer);
+
 });
 
 //adding a click event listener to the interval button; the callback function is what occurs on click
 intervalButton.addEventListener('click', () => {
   //the number we are starting to count from
-  let count = 5;
+
   //disabling the interval button upon click
-  intervalButton.style.pointerEvents = 'none';
+
 
   /*
     variable to hold  the start of our interval, function executes every second
     */
-  let countDown = setInterval(() => {
-    //decrement the number displayed to the user to indicated the interval
-    intervalDisplay.textContent = count--;
-    //what happens when the interval reaches zero
-    if (count < 0) {
-      //enable the interval button
-      intervalButton.style.pointerEvents = 'auto';
-      //stop the interval timer with the stop variable as its argument
-      clearInterval(countDown);
-    }
-  }, 1000);
+
+  //decrement the number displayed to the user to indicated the interval
+
+  //what happens when the interval reaches zero
+
+  //enable the interval button
+
+  //stop the interval timer with the stop variable as its argument
+
+
 });
